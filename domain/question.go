f2a720f405fd4aaebda38a5f28ffbfc5a639d580
Masahiko Sato <sato_m@mediaxis.jp>
2019-11-18 11:32:59 +0900

    カテゴリのエンティティを追加

--- domain/question.go
+++ domain/question.go
@@ -1,9 +1,10 @@
 package domain
 
 type Question struct {
-	ID int
-	Title string
-	Body string
+	ID int `json:"id"`
+	Title string `json:"title"`
+	Body string `json:"body"`
+	Category Category `json:"category"`
 }
 
 type Questions = []Question 
--- interfaces/database/question/question_repository.go
+++ interfaces/database/question/question_repository.go
@@ -8,8 +8,8 @@ type QuestionRepository struct {
 
 func (r QuestionRepository) FetchList() domain.Questions {
 	questions := domain.Questions{
-		domain.Question{ID: 1, Title: "title1", Body: "body1"},
-		domain.Question{ID: 2, Title: "title2", Body: "body2"},
+		domain.Question{ID: 1, Title: "title1", Body: "body1", Category: domain.Category{ID: 1, Name: "カテゴリ1"}},
+		domain.Question{ID: 2, Title: "title2", Body: "body2", Category: domain.Category{ID: 2, Name: "カテゴリ2"}},
 	}
 	return questions
 }
